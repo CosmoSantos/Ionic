81f41da73479637b1826ae7b98b2f85d9904bc62
CosmoSantos <cosmosantos13@gmail.com>
2018-04-28 22:14:57 -0300

    Ionic com firebase

---
+++ src/app/firebase.credencial.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CREDENCIAL = {
+    apiKey: "AIzaSyB5TAhtwacJhVBftlSNGToDhfzKbknODdw",
+    authDomain: "planejamentoviagem-9390b.firebaseapp.com",
+    databaseURL: "https://planejamentoviagem-9390b.firebaseio.com",
+    projectId: "planejamentoviagem-9390b",
+    storageBucket: "planejamentoviagem-9390b.appspot.com",
+    messagingSenderId: "152251833562"
+};
---
+++ src/pages/home/home.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ContactPage } from '../contact/contact'; 
+import { AngularFireDatabase } from 'angularfire2/database';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  site = { 
+    url: 'Teste firebase',
+    descripton: 'Teste ionic 3 com firebase'
+  }
+
+  constructor(public navCtrl: NavController, public db: AngularFireDatabase) {
+
+    this.db.list('site').push(this.site)
+  }
+
+  contato() {
+    this.navCtrl.push(ContactPage);
+  }
+
+}
